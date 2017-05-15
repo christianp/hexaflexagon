f1c7375cf291c8a4c51f34b83c9b792d76123f28
Christian Lawson-Perfect <christianperfect@gmail.com>
2017-05-15 12:46:35 +0100

    set the clip path on the preview properly

--- thing.js
+++ thing.js
@@ -13,17 +13,19 @@ Thinger.prototype = {
     });
 });
 
+var app;
 fetch('trihexaflexagon-template.svg').then(function(r) {return r.text();}).then(function(source) {
 
 var svg = source.replace(/\s*\n\s*/mg,' ').replace(/^.*<svg/,'<svg');
 console.log(svg.slice(0,500));
 document.getElementById('svg-container').innerHTML = svg;
 
-var app = new Vue({
+app = new Vue({
     el: '#app',
     data: {
         data_uri: null,
-        mode: 'take-photo'
+        mode: 'take-photo',
+        name: ''
     },
     methods: {
         take_snapshot: function() {
@@ -39,6 +41,35 @@ var app = new Vue({
         },
         confirm_photo: function() {
             this.mode = 'show-hexaflexagon';
+        },
+
+        set_clip_path: function() {
+            let result_container = document.querySelector('#result-container');
+            let rect = result_container.getBoundingClientRect();
+            var width = rect.width;
+            var height = rect.height;
+            var r = height/2;
+            var h = Math.sqrt(3)*r/2;
+            var x1 = width/2-h;
+            var x2 = width/2;
+            var x3 = width/2+h;
+            var y1 = 2*r;
+            var y2 = 3*r/2;
+            var y3 = r/2;
+            var y4 = 0;
+            var clip_path = 'polygon('+x1+'px '+y2+'px, '+x2+'px '+y1+'px, '+x3+'px '+y2+'px, '+x3+'px '+y3+'px, '+x2+'px '+y4+'px, '+x1+'px '+y3+'px)';
+            document.getElementById('result-container').style.clipPath = clip_path;
+
+            document.body.addEventListener('drop',function(e) {
+                var files = e.dataTransfer.files;
+                var f = files[0];
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    app.data_uri = e.target.result;
+                }
+                reader.readAsDataURL(f);
+            });
+
         }
     },
     watch: {
@@ -49,6 +80,11 @@ var app = new Vue({
                 var segment = document.querySelector('svg #segment-'+i);
                 segment.setAttributeNS('http://www.w3.org/1999/xlink','xlink:href',app.data_uri);
             });
+            this.set_clip_path();
+        },
+        name: function() {
+            var name = document.querySelector('svg #name tspan tspan');
+            name.innerHTML = app.name;
         }
     },
     mounted: function() {
@@ -62,32 +98,8 @@ var app = new Vue({
             crop_width: 480,
             crop_height: 480
         });
-        let camera_container = document.getElementById('camera');
-        let rect = camera_container.getBoundingClientRect();
-        var width = rect.width;
-        var height = rect.height;
-        var r=height/2/Math.sqrt(3);
-        var x1=width/2-2*r;
-        var x2=width/2-r;
-        var x3=width/2+r;
-        var x4=width/2+2*r;
-        var y1=height;
-        var y2=height/2;
-        var y3=0;
-        var clip_path = 'polygon('+x1+'px '+y2+'px, '+x2+'px '+y1+'px, '+x3+'px '+y1+'px, '+x4+'px '+y2+'px, '+x3+'px '+y3+'px, '+x2+'px '+y3+'px)';
-        camera_container.style.clipPath = clip_path;
-        document.getElementById('result-container').style.clipPath = clip_path;
 
-        document.body.addEventListener('drop',function(e) {
-            var files = e.dataTransfer.files;
-            var f = files[0];
-            var reader = new FileReader();
-            reader.onload = function(e) {
-                app.data_uri = e.target.result;
-            }
-            reader.readAsDataURL(f);
-        });
-
+        this.set_clip_path();
     }
 });
 
